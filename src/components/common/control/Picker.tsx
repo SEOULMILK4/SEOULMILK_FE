09415b5a48129dd5be7c3d73a35f7b0fac464f0a
yeona813 <aya813@naver.com>
2025-02-24 20:11:58 +0900

    #7 [feat] : Picker 수정

--- src/components/common/control/Picker.tsx
+++ src/components/common/control/Picker.tsx
@@ -1,23 +1,48 @@
 import { useState } from "react";
 
 interface PickerProps {
+  totalCount: number;
+  pendingCount?: number;
   correctCount: number;
   inCorrectCount: number;
 }
 
 enum PickType {
+  TOTAL = "전체",
+  PENDING = "대기",
   CORRECT = "일치",
   INCORRECT = "불일치",
 }
 
 /**
  *
+ * @param totalCount - 전체 건수
+ * @param pendingCount - (optional) 결재자 지급결의서 결재함에서 대기 건수
  * @param correctCount - 일치 건수
  * @param inCorrectCount - 불일치 건수
  * @returns
  */
-const Picker = ({ correctCount, inCorrectCount }: PickerProps) => {
+const Picker = ({
+  totalCount,
+  pendingCount,
+  correctCount,
+  inCorrectCount,
+}: PickerProps) => {
   const options = [
+    {
+      type: PickType.TOTAL,
+      count: totalCount,
+      activeColor: "text-grayScale-600",
+    },
+    ...(pendingCount
+      ? [
+          {
+            type: PickType.PENDING,
+            count: pendingCount,
+            activeColor: "text-warning-400",
+          },
+        ]
+      : []),
     {
       type: PickType.CORRECT,
       count: correctCount,
@@ -32,21 +57,23 @@ const Picker = ({ correctCount, inCorrectCount }: PickerProps) => {
 
   const [currentPick, setCurrentPick] = useState(PickType.CORRECT);
 
+  const currentIndex = options.findIndex(
+    (option) => option.type === currentPick
+  );
+  const translateXValue = `${currentIndex * 100}%`;
+
   return (
-    <div className="relative flex h-fit rounded-lg w-[256px] p-[2px] bg-grayScale-100 overflow-hidden">
+    <div className="relative flex h-fit rounded-lg w-fit p-[2px] bg-grayScale-100 overflow-hidden">
       <div
-        className="absolute top-[2px] left-[2px] w-[126px] h-[28px] bg-white rounded-[7px] transition-transform duration-300 ease-in-out"
+        className="absolute top-[2px] left-[2px] w-[100px] h-[28px] bg-white rounded-[7px] transition-transform duration-300 ease-in-out"
         style={{
-          transform:
-            currentPick === PickType.INCORRECT
-              ? "translateX(100%)"
-              : "translateX(0%)",
+          transform: `translateX(${translateXValue})`,
         }}
       />
       {options.map(({ type, count, activeColor }) => (
         <div
           key={type}
-          className={`center relative w-[126px] h-[28px] text-b3 font-medium rounded-[7px]
+          className={`center relative w-[100px] h-[28px] text-b3 font-medium rounded-[7px]
             ${currentPick === type ? `${activeColor} font-semibold` : "text-grayScale-500"}`}
           onClick={() => setCurrentPick(type)}
         >
