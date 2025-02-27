381fd837803eaf4f647f7fb0109fae1f25530844
choihooo <hochoi8621@naver.com>
2025-02-27 10:42:18 +0900

    #8 [feat] : grayScale 600 버튼 추가

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface ButtonProps {
   size: "medium" | "large";
-  color: "green" | "gray";
+  color: "green" | "gray" | "black";
   disabled: boolean;
   children: React.ReactNode;
 }
@@ -10,7 +10,7 @@ interface ButtonProps {
 /**
  *
  * @param size - "medium" | "large"
- * @param color - "green" | "gray"
+ * @param color - "green" | "gray" | "black"
  * @param disabled - true or false
  *
  * @returns
@@ -23,17 +23,30 @@ const Button = ({ size, color, disabled, children }: ButtonProps) => {
       : "w-[97px] h-12 text-b1 font-bold";
   };
 
-  // 버튼 색상 및 상태에 따른 스타일을 설정하는 함수
+
+
   const getColorClass = () => {
     if (disabled) {
-      return color === "green"
-        ? "bg-secondary-50 text-white"
-        : "bg-grayScale-50 text-grayScale-300";
+      switch (color) {
+        case "green":
+          return "bg-secondary-50 text-white";
+        case "black":
+          return "bg-grayScale-400 text-grayScale-300";  // black 색상에 대한 비활성화 스타일 추가
+        default:
+          return "bg-grayScale-50 text-grayScale-300";
+      }
+    } else {
+      switch (color) {
+        case "green":
+          return "bg-secondary-500 hover:bg-secondary-700 text-white";
+        case "black":
+          return "bg-grayScale-600 hover:bg-grayScale-700 text-white";  // black 색상에 대한 활성화 스타일 추가
+        default:
+          return "bg-grayScale-100 hover:bg-grayScale-300 text-grayScale-600";
+      }
     }
-    return color === "green"
-      ? `bg-secondary-500 hover:bg-secondary-700 text-white`
-      : `bg-grayScale-100 hover:bg-grayScale-300 text-grayScale-600`;
   };
+  
 
   return (
     <button
