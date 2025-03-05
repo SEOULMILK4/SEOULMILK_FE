83b5950a508d0ef4ef21766dfcabb16f4817f88f
yeona813 <aya813@naver.com>
2025-03-06 01:58:08 +0900

    #26 [feat] : ConvertModal - step3 구현

---
+++ src/components/common/control/StatusBadge.tsx
@@ -0,0 +1,25 @@
+interface StatusBadgeProps {
+  status: "성공" | "실패" | "제외";
+  count: number;
+}
+
+const StatusBadge = ({ status, count }: StatusBadgeProps) => {
+  const statusStyles = {
+    성공: "bg-secondary-25 text-secondary-500",
+    실패: "bg-primary-50 text-primary-500",
+    제외: "bg-grayScale-50 text-grayScale-500",
+  };
+
+  return (
+    <div className="flex gap-[6px]">
+      <div
+        className={`px-[10px] py-[1px] w-[47px] b3 rounded text-center ${statusStyles[status]}`}
+      >
+        {status}
+      </div>
+      <p className="b4 text-grayScale-700">{count}건</p>
+    </div>
+  );
+};
+
+export default StatusBadge;
