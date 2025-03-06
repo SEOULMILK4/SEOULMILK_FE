b10aaced8b1caaa4aba8a05964fea3a8a306e669
yeona813 <aya813@naver.com>
2025-03-07 00:55:15 +0900

    #26 [feat] : SubmitPage 구현

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
+    // 이 부분 고쳐야함..!! 스크롤 생기는 것 싫어서 일단 둠
     <div className="flex flex-col w-screen h-screen overflow-hidden">
       <NavigationBar />
       <Outlet />
