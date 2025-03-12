b243ca4b64b5c495c3533b799b0a4c6a45c54a22
yeona813 <aya813@naver.com>
2025-03-12 15:30:24 +0900

    #38 [feat] : 관리자 - 계산서 통합 제출 조회 API 일부 연결

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -1,3 +1,4 @@
+import { useUserStore } from "@/stores/useUserStore";
 import { api } from "./index";
 
 /**
@@ -13,12 +14,66 @@ export const postAdminLogin = async (masterKey: string) => {
     });
 
     if (response.data) {
+      localStorage.setItem("role", "admin");
       localStorage.setItem("accessToken", response.data.data.accessToken);
       localStorage.setItem("refreshToken", response.data.data.refreshToken);
+
+      useUserStore.getState().setUser({
+        role: "admin",
+      });
       return true;
     }
   } catch (error) {
     console.log(error);
     return false;
+  }
+};
+
+export const getAdminNtsTax = async (page: number, status?: string) => {
+  const accessToken = localStorage.getItem("accessToken");
+
+  const params = new URLSearchParams();
+  params.append("page", page.toString());
+  if (status) {
+    params.append("status", status);
+  }
+
+  try {
+    const response = await api.get(`/admin/nts-tax?${params.toString()}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (response.data) {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * 관리자자 - 세금 계산서 페이지 삭제
+ *
+ * @param deleteNtsTaxIds
+ * @returns
+ */
+export const deleteNtsTaxIds = async (ntsTaxId: number[]) => {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+
+    const response = await api.delete("/admin/nts-tax", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: { ntsTaxId: ntsTaxId },
+    });
+
+    if (response.data) {
+      return response.data.success;
+    }
+  } catch (error) {
+    console.error("다중 삭제 API 호출 중 오류 발생:", error);
   }
 };
--- src/pages/AdminHubPage.tsx
+++ src/pages/AdminHubPage.tsx
@@ -1,5 +1,109 @@
+import { deleteNtsTaxIds, getAdminNtsTax } from "@/api/admin";
+import AdminHubHeader from "@/components/adminHub/AdminHubHeader";
+import AdminHubTable from "@/components/adminHub/AdminHubTable";
+import Pagination from "@/components/common/control/Pagination";
+import CheckModal from "@/components/submit/modal/CheckModal";
+import { useAdminPickerStore } from "@/stores/useAdminStore";
+import useModalStore from "@/stores/useModalStore";
+import { NtsTaxHubData } from "@/types/ntsTax";
+import { useEffect, useState } from "react";
+
 const AdminHubPage = () => {
-  return <div></div>;
+  const [data, setData] = useState<NtsTaxHubData | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [checkedItem, setCheckedItem] = useState<number[]>([]);
+  const [isAllChecked, setIsAllChecked] = useState(false);
+  const [openInfo, setOpenInfo] = useState(false);
+  const { currentStatus } = useAdminPickerStore();
+  const { isSaveCheckOpen, closeSaveCheck } = useModalStore();
+
+  const fetchData = async () => {
+    const response = await getAdminNtsTax(currentPage - 1, currentStatus);
+    if (response) {
+      setData(response);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const success = await deleteNtsTaxIds(checkedItem);
+      if (success) {
+        closeSaveCheck();
+        setCheckedItem([]);
+        setIsAllChecked(false);
+        setOpenInfo(false);
+        fetchData();
+        setData((prevData) =>
+          prevData
+            ? {
+                ...prevData,
+                ntsTaxList: prevData.ntsTaxList.filter(
+                  (item) => !checkedItem.includes(item.ntsTaxId!)
+                ),
+              }
+            : null
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      setCheckedItem([]);
+      setIsAllChecked(false);
+      setOpenInfo(false);
+      closeSaveCheck();
+      fetchData();
+    } catch (error) {
+      console.error("제출 실패:", error);
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [currentStatus, currentPage]);
+
+  return (
+    <div className="relative flex flex-col items-center w-full h-full gap-4 bg-grayScale-25">
+      <AdminHubHeader
+        totalCount={data?.totalCnt || 0}
+        correctCount={data?.approvedCnt || 0}
+        inCorrectCount={data?.rejectedCnt || 0}
+        checkedItem={checkedItem}
+      />
+      {data ? (
+        <AdminHubTable
+          data={data?.ntsTaxList ?? []}
+          checkedItem={checkedItem}
+          setCheckedItem={setCheckedItem}
+          totalCount={data.totalCnt}
+          correctCount={data.approvedCnt}
+          inCorrectCount={data.rejectedCnt}
+          isAllChecked={isAllChecked}
+          setIsAllChecked={setIsAllChecked}
+          openInfo={openInfo}
+          setOpenInfo={setOpenInfo}
+        />
+      ) : (
+        <p>데이터 없음</p>
+      )}
+      <Pagination
+        totalPage={data?.totalPage || 1}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      {isSaveCheckOpen && (
+        <CheckModal
+          count={checkedItem.length}
+          onDelete={handleDelete}
+          onSubmit={handleSubmit}
+        />
+      )}
+    </div>
+  );
 };
 
 export default AdminHubPage;
